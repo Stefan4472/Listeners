bf95ab1ca8743839003cb6b1df545e326256cd9e
DJStefan <Stefan4472@users.noreply.github.com>
2015-02-07 14:46:37 -0500

    Implemented Undo/Redo
    Currently basic functions, one character at a time

--- GUI.java
+++ GUI.java
@@ -1,20 +1,24 @@
 import javax.swing.*;
-import javax.swing.event.CaretEvent;
-import javax.swing.event.CaretListener;
-import javax.swing.event.DocumentEvent;
-import javax.swing.event.DocumentListener;
+import javax.swing.event.*;
+import javax.swing.undo.CannotRedoException;
+import javax.swing.undo.CannotUndoException;
+import javax.swing.undo.UndoManager;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.beans.PropertyChangeListener;
+import java.awt.event.KeyEvent;
 
 public class GUI extends JFrame {
     private JLabel info = new JLabel("Info: ");
     private JTextField text_box = new JTextField(12);
     private JLabel caret_info = new JLabel("Caret info will be displayed here");
     private JLabel change_info = new JLabel("Change info will be displayed here");
+    private JLabel undo_info = new JLabel("Undo info will be displayed here");
     private JLabel enter_info = new JLabel();
     private JButton go_button = new JButton("Go!");
+    private JButton undo_button = new JButton("Undo");
+    private JButton redo_button = new JButton("Redo");
+    protected UndoManager undo = new UndoManager(); /* manager for undo/redo support */
     public GUI() {
         JFrame frame = new JFrame();
         frame.setLayout(new FlowLayout(FlowLayout.LEADING));
@@ -24,14 +28,17 @@ public class GUI extends JFrame {
         frame.add(caret_info);
         frame.add(change_info);
         frame.add(enter_info);
+        frame.add(enter_info);
+        frame.add(undo_button);
+        frame.add(redo_button);
         frame.setTitle("Textbox");
-        frame.setSize(225, 150);
+        frame.setSize(225, 170);
         frame.setComponentOrientation(
                 ComponentOrientation.UNKNOWN);
-        frame.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        frame.setVisible(true);
-        pack();
-        frame.setLocationRelativeTo(null);
+        frame.setDefaultCloseOperation(EXIT_ON_CLOSE); /* so process finishes when user closes window */
+        frame.setVisible(true); /* makes frame visible */
+        pack(); /* figures everything out, basically */
+        frame.setLocationRelativeTo(null); /* centers on screen (must be after pack statement!!!) */
         text_box.addCaretListener(new CaretListener() {
             @Override
             public void caretUpdate(CaretEvent e) {
@@ -63,8 +70,7 @@ public class GUI extends JFrame {
                 /* only used in styled components */
             }
         });
-        //text_box.getDocument().addDocumentListener(new MyDocumentListener());
-        text_box.addActionListener(new ActionListener() {
+        text_box.addActionListener(new ActionListener() { /* also check out JRootPane */
             @Override
             public void actionPerformed(ActionEvent e) {
                 change_info.setText("Enter key pressed");
@@ -74,6 +80,37 @@ public class GUI extends JFrame {
             @Override
             public void actionPerformed(ActionEvent e) {
                 change_info.setText("Button pressed");
+            }
+        });
+        text_box.getDocument().addUndoableEditListener(new UndoableEditListener() {
+            @Override
+            public void undoableEditHappened(UndoableEditEvent e) {
+                undo.addEdit(e.getEdit());
+               // undoAction.updateUndoState();
+               // redoAction.updateRedoState();
+            }
+        });
+        undo_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    undo.undo();
+                    undo_info.setText("Undid");
+                } catch (CannotUndoException ex) {
+                    undo_info.setText("Unable to undo: " + ex);
+                    ex.printStackTrace();
+                }
+            }
+        });
+        redo_button.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    undo.redo();
+                } catch (CannotRedoException ex) {
+                    undo_info.setText("Unable to redo: " + ex);
+                    ex.printStackTrace();
+                }
             }
         });
     }
